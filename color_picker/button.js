9c980bbf09a417dbaef95ce3f388f20754b31d53
thegirlfromipanema <mayanovarini@gmail.com>
2018-02-17 22:39:23 -0800

    incorporate the event handler on the button and set the prop

--- color_picker/button.js
+++ color_picker/button.js
@@ -4,6 +4,7 @@ export class Button extends React.Component {
 	render() {
 		return (
 			<button
+        onClick={this.props.onClick}
 				className={ this.props.light ? 'light-button' : 'dark-button' }>
 				Refresh
 			</button>
